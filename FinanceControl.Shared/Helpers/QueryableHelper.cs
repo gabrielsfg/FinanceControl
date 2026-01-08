bc5c572c6ba84cf75a128ace67640f11f9b85b75
Gabrielsfg <gabriellfsg00@gmail.com>
2026-01-08 01:20:32 -0300

    Queryable helper

---
+++ FinanceControl.Shared/Helpers/QueryableHelper.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinanceControl.Shared.Helpers
+{
+    public static class QueryableHelper
+    {
+        public static IQueryable<T> WhereIf<T>(
+            this IQueryable<T> queryable,
+            bool condition,
+            Expression<Func<T, bool>> predicate)
+        {
+            if(condition)
+            {
+                return queryable.Where(predicate);
+            }
+
+            return queryable;
+        }
+    }
+}
