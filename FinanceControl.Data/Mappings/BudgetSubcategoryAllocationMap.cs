78f5ef010547f5ac28e35d0726a4964dc9cabd11
Gabrielsfg <gabriellfsg00@gmail.com>
2026-01-24 19:01:06 -0300

    Start budgetSubcategorryAllocation

---
+++ FinanceControl.Data/Mappings/BudgetSubcategoryAllocationMap.cs
@@ -0,0 +1,36 @@
+﻿using FinanceControl.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinanceControl.Data.Mappings
+{
+    public class BudgetSubcategoryAllocationMap : IEntityTypeConfiguration<BudgetSubcategoryAllocation>
+    {
+        public void Configure(EntityTypeBuilder<BudgetSubcategoryAllocation> builder)
+        {
+            builder.ToTable("BudgetSubcategoryAllocations");
+            builder.HasKey(bsa => bsa.Id);
+            builder.Property(bsa => bsa.BudgetId);
+            builder.Property(bsa => bsa.AreaId);
+            builder.Property(bsa => bsa.SubCategoryId);
+            builder.Property(bsa => bsa.ExpectedValue);
+            builder.Property(bsa => bsa.CreatedAt)
+                .HasColumnType("timestamp without time zone")
+                .HasDefaultValueSql("timezone('America/Sao_Paulo', now())")
+                .IsRequired()
+                .ValueGeneratedOnAdd();
+            builder.Property(bsa => bsa.UpdatedAt)
+                .HasColumnType("timestamp without time zone")
+                .ValueGeneratedOnAdd();
+
+            builder.HasOne(bsa => bsa.Budget)
+                .WithMany()
+                .HasForeignKey()
+        }
+    }
+}
--- FinanceControl.Domain/Entities/Budget.cs
+++ FinanceControl.Domain/Entities/Budget.cs
@@ -5,9 +5,11 @@ namespace FinanceControl.Domain.Entities
 {
     public class Budget : OwnedEntity
     {
-        public string Name { get; set; }
+        public string Name { get; set; } = string.Empty;
         public int StartDate {  get; set; }
         public EnumBudgetRecurrence Reccurrence { get; set; }
+
+        public ICollection<BudgetSubcategoryAllocation> BudgetSubcategoryAllocations { get; set; } = [];
 
     }
 }
